ba3672d073305cce56110360abf46a801c715a14
AzhirAhmadi <aaahmadi99@gmail.com>
2019-08-01 11:09:46 +0430

    -add Pic model -add annotate gem

---
+++ app/models/pic.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: pics
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Pic < ApplicationRecord
+end
