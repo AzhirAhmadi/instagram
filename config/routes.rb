4ab85a2e66066c3ffe2b55601b6489eb3267ff97
AzhirAhmadi <aaahmadi99@gmail.com>
2019-08-01 11:15:08 +0430

    -add pics controller -add pics resources to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 # == Route Map
 #
-#                    Prefix Verb URI Pattern                                                                              Controller#Action
-#        rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-# rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#        rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-# update_rails_disk_service PUT  /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#      rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+#                    Prefix Verb   URI Pattern                                                                              Controller#Action
+#                      pics GET    /pics(.:format)                                                                          pics#index
+#                           POST   /pics(.:format)                                                                          pics#create
+#                   new_pic GET    /pics/new(.:format)                                                                      pics#new
+#                  edit_pic GET    /pics/:id/edit(.:format)                                                                 pics#edit
+#                       pic GET    /pics/:id(.:format)                                                                      pics#show
+#                           PATCH  /pics/:id(.:format)                                                                      pics#update
+#                           PUT    /pics/:id(.:format)                                                                      pics#update
+#                           DELETE /pics/:id(.:format)                                                                      pics#destroy
+#                      root GET    /                                                                                        pics#index
+#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pics
+
+  root "pics#index"
 end
